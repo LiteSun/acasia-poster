02d89519233681d10f1da8b7bd86764094b5837b
litesun <7sunmiao@gmail.com>
2021-09-17 16:40:03 +0800

    update

--- nuxt.config.js
+++ nuxt.config.js
@@ -55,6 +55,6 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
-    transpile: [/^element-ui/],
+    transpile: [/^element-ui/, /^vue2-google-maps($|\/)/],
   }
 }
